88c4db389b6f88f31db38ffdb2b14adf540a2f8d
ronit kissis <ronitkisis1997@gmail.com>
2022-08-09 17:27:18 +0300

    fix: tests work with js-logger

--- tests/integration/testContainerConfig.ts
+++ tests/integration/testContainerConfig.ts
@@ -1,16 +1,11 @@
-import { readFileSync } from 'fs';
 import { container } from 'tsyringe';
 import config from 'config';
-import { MCLogger, IServiceConfig } from '@map-colonies/mc-logger';
+import jsLogger from '@map-colonies/js-logger';
 import { SERVICES } from '../../src/common/constants';
 
 function registerTestValues(): void {
-  const packageContent = readFileSync('./package.json', 'utf8');
-  const service = JSON.parse(packageContent) as IServiceConfig;
-  const logger = new MCLogger({ log2console: true, level: 'error' }, service);
-
   container.register(SERVICES.CONFIG, { useValue: config });
-  container.register(SERVICES.LOGGER, { useValue: logger });
+  container.register(SERVICES.LOGGER, { useValue: jsLogger({ enabled: false }) });
 }
 
 export { registerTestValues };

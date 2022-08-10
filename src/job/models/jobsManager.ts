588589e8018fb7405c145d218d0224e90f271019
ronit kissis <ronitkisis1997@gmail.com>
2022-08-10 13:38:49 +0300

    feat: seperated log levels

--- src/job/models/jobsManager.ts
+++ src/job/models/jobsManager.ts
@@ -12,26 +12,31 @@ export class JobsManager {
   public constructor(@inject(SERVICES.CONFIG) private readonly config: IConfig, @inject(SERVICES.LOGGER) private readonly logger: Logger) {}
 
   public async createJob(payload: JobPayload): Promise<Job> {
-    this.logger.info(`Create a new job: ${JSON.stringify(payload)}`);
     payload.version = '1';
     payload.type = '3D';
     payload.description = '3D Model Ingestion';
     payload.percentage = 0;
     payload.tasks = [];
+    this.logger.debug({ msg: 'Create a new job', jobPayload: payload });
     const url = this.config.get<string>('jobUrl');
     try {
       const response = await axios.post<Job>(url, payload);
       return response.data;
     } catch (error) {
-      this.logger.error(`Creating a new job failed with error messege: ${JSON.stringify(error)}`);
+      this.logger.error({ msg: 'Creating a new job failed with error messege', error });
       throw error;
     }
   }
 
   public async updateJobStatus(jobId: string, payload: JobUpdatePayload): Promise<void> {
-    this.logger.info(`Updating job status. jobId:${jobId} ,JobStatus: ${payload.status}`);
+    this.logger.debug({ msg: 'Updating job status', jobId: jobId, jobStatus: payload.status });
     const url = this.config.get<string>('jobUrl');
     const updateUrl = `${url}/${jobId}`;
-    await axios.put(updateUrl, payload);
+    try {
+      await axios.put(updateUrl, payload);
+      this.logger.info({ msg: 'Success in updating job status', jobId: jobId, jobStatus: payload.status });
+    } catch (error) {
+      this.logger.error({ msg: 'Failed in updating job status', jobId: jobId, jobStatus: payload.status });
+    }
   }
 }
--- src/validator/models/validationManager.ts
+++ src/validator/models/validationManager.ts
@@ -3,13 +3,14 @@ import { Logger } from '@map-colonies/js-logger';
 import { SERVICES } from '../../common/constants';
 import { IConfig } from '../../common/interfaces';
 import { Payload } from '../../common/models/payload';
+import { ModelPathNotSupported } from '../../common/models/errors';
 
 @injectable()
 export class ValidationManager {
   public constructor(@inject(SERVICES.CONFIG) private readonly config: IConfig, @inject(SERVICES.LOGGER) private readonly logger: Logger) {}
 
   public validateModelPath(payload: Payload): Payload {
-    this.logger.info(`validation model path: recieved: ${JSON.stringify(payload['modelPath'])}`);
+    this.logger.debug({ msg: 'validating model path', recieved: payload['modelPath'] });
     const basePath = this.config.get<string>('paths.basePath');
     const mountPath = this.config.get<string>('paths.mountPath');
     if (payload['modelPath'].includes(basePath)) {
@@ -17,6 +18,7 @@ export class ValidationManager {
       payload['modelPath'] = newModelPath;
       return payload;
     }
-    throw new Error('Unknown model path- the model isnt in the agreed folder');
+    this.logger.error({ msg: 'Unknown model path- the model isnt in the agreed folder', recieved: basePath, expected: mountPath });
+    throw new ModelPathNotSupported('unsupported model path');
   }
 }
